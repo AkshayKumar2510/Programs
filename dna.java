6b6eb7d99e5d6f78e62aa733a3686261f8536129
Akshay Kumar <67497196+AkshayKumar2510@users.noreply.github.com>
2021-10-08 10:26:49 +0530

    Add files via upload
    Printing DNA Pattern using Java

---
+++ dna.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.io.*;
+class dna
+{	public static void main(String...args)
+	{ int n;
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter number: ");
+	 n=sc.nextInt();
+	for (int z=1;z<6;z++)
+	{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=n-i;j>=0;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=0;j<=i;j++)
+		{
+			System.out.print("* ");
+		}
+	System.out.println();
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(i==n-1)
+		{break;}
+		for(int j=0;j<=i+1;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=n;j>i;j--)
+		{
+			System.out.print("* ");
+		}
+	System.out.println();
+	}
+	//tail
+	/*for(int z=1;z<3;z++)
+	{	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=n-i;j>=1;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=0;j<=2;j++)
+		{
+			System.out.print("*");
+		}
+	System.out.println();
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=0;j<=2;j++)
+		{
+			System.out.print("*");
+		}
+	System.out.println();
+	}
+	}
+*/
+	}
+	}
+}
+
+/*
+Enter number:
+4
+     *
+    * *
+   * * *
+  * * * *
+  * * * *
+   * * *
+    * *
+     *
+    * *
+   * * *
+  * * * *
+  * * * *
+   * * *
+    * *
+     *
+    * *
+   * * *
+  * * * *
+  * * * *
+   * * *
+    * *
+     *
+    * *
+   * * *
+  * * * *
+  * * * *
+   * * *
+    * *
+*/
